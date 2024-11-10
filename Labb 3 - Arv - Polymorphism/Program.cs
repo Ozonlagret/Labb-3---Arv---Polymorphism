8690596c239f90dcb445261169fdb3e099f37f89
Ozonlagret <tim.drejhede@gmail.com>
2024-11-10 20:52:49 +0100

    Lade till kommentar

--- Labb 3 - Arv - Polymorphism/Program.cs
+++ Labb 3 - Arv - Polymorphism/Program.cs
@@ -5,13 +5,14 @@
         static void Main(string[] args)
         {
             // Här används Geometry för att initiera olika klasser som har arvt från den
-            // Fråga: Har det någon betydelse 
+            // Fråga: Har det någon betydelse om man sätter Geometry istället för respektive underklass först?
             Geometry square = new Square();
             Geometry circle = new Circle();
             Geometry rectangle = new Rectangle();
 
             Geometry[] shapes = { square, circle, rectangle };
 
+            // Får ut flera värden med en simpel loop för att klasserna har gemensamma objekt med olika värden
             foreach (Geometry shape in shapes)
             {
                 Console.WriteLine("The area of the " + shape + " is " + shape.Area());
